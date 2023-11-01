222f24c7fc33b7ee637feb85a8a4e83774eadc4a
Hongda <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-01 00:06:25 -0500

    finished bottom nav

--- src/components/PurpleBookMainPage/PurpleBookMainPage.jsx
+++ src/components/PurpleBookMainPage/PurpleBookMainPage.jsx
@@ -1,73 +1,36 @@
 import { firebaseSignOut, useProfile } from "../../utilities/firebaseUtils";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown";
 import data from "../../utilities/temp.json";
+import PurpleBookButtomNav from "../PurpleBookButtomNav/PurpleBookButtomNav";
 import "./PurpleBookMainPage.less";
 
 const PurpleBookMainPage = ({ setIsUserLoggedIn }) => {
-  const handleFirebaseLogout = () => {
-    setIsUserLoggedIn(false);
-    firebaseSignOut();
-  };
+  const navigate = useNavigate();
 
   const [user, isAdmin] = useProfile();
   console.log(user);
   console.log(isAdmin);
 
-  const [selectedSport, setSelectedSport] = useState("basketball");
-  const [selectedCourt, setSelectedCourt] = useState();
-
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate(`/place/${selectedCourt}`, {
-      state: data.sport[selectedSport][selectedCourt],
-    });
+  const handleFirebaseLogout = () => {
+    setIsUserLoggedIn(false);
+    firebaseSignOut();
   };
 
-  const handleSelectSport = (eventKey, event) => {
-    setSelectedSport(eventKey);
-  };
-
-  const handleSelectCourt = (eventKey, event) => {
-    setSelectedCourt(eventKey);
-  };
   return (
-    <div>
-      <h1>You have selected: {selectedSport}</h1>
-      <Dropdown onSelect={handleSelectSport}>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Choose a sport
-        </Dropdown.Toggle>
+    <div className="main-page">
+      <div className="main-page-header">
+        <p className="main-page-title"> <span className="purple">Purple</span>Book</p>
+        <p className="main-page-slogan">
+          Book Your Next Court With PurpleBook !
+        </p>
+      </div>
+      <div className="main-page-content">
 
-        <Dropdown.Menu className="my-dropdown-menu">
-          <Dropdown.Item eventKey="basketball">basketball</Dropdown.Item>
-          <Dropdown.Item eventKey="tennis">tennis</Dropdown.Item>
-          <Dropdown.Item eventKey="badminton">badminton</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-
-      <Dropdown onSelect={handleSelectCourt}>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Choose a {selectedSport} court
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu className="dropbar-menu">
-          {Object.entries(data.sport[selectedSport]).map(([key, sportInfo]) => (
-            <Dropdown.Item eventKey={key} key={key}>
-              {key}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-      {selectedCourt === undefined ? (
-        <p>please select a court</p>
-      ) : (
-        <button onClick={handleButtonClick}>Go to selected court</button>
-      )}
-
+        
+      </div>
       <button onClick={handleFirebaseLogout}>Sign Out</button>
+      <PurpleBookButtomNav />
     </div>
   );
 };
